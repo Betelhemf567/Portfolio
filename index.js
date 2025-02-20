0d95b9f9ad302fc41db170aa82e7e0fa9f1f3a07
Betelhemf567 <b.chelebo@alustudent.com>
2025-02-20 16:24:00 +0200

    this is my index.js file

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    const navLinks = document.querySelector(".navlinks");
+    navLinks.classList.toggle("active");
+});
