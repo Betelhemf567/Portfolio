f01b0fc6e22326269975aa0e5b2a1b7afb2513c0
Betelhemf567 <b.chelebo@alustudent.com>
2025-02-20 17:43:11 +0200

    updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Navbar toggle implementation
 document.getElementById("menu-toggle").addEventListener("click", function () {
     const navLinks = document.querySelector(".navlinks");
     navLinks.classList.toggle("active");
